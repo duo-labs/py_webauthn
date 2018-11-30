3507eb900aa70ad0a30fd9056bc0a3a3ff68b6fe
James Barclay <futureimperfect@users.noreply.github.com>
2018-11-30 18:41:07 -0500

    Incremented version to 0.4.

--- webauthn/__init__.py
+++ webauthn/__init__.py
@@ -6,4 +6,4 @@ from .webauthn import WebAuthnMakeCredentialOptions
 from .webauthn import WebAuthnRegistrationResponse
 from .webauthn import WebAuthnUser
 
-__version__ = '0.3.1'
+__version__ = '0.4'
