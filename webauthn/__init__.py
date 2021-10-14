a815a2d6ea1ecee0f2e6d4c1ab04d7d2abd6386b
Matthew Miller <mmiller@duosecurity.com>
2021-10-14 15:42:20 -0700

    Form core API of library

--- webauthn/__init__.py
+++ webauthn/__init__.py
@@ -1,1 +1,11 @@
+from .registration import (
+    generate_registration_options,
+    verify_registration_response,
+)
+from .authentication import (
+    generate_authentication_options,
+    verify_authentication_response,
+)
+from .helpers import base64url_to_bytes, options_to_json
+
 __version__ = "1.0.0"
