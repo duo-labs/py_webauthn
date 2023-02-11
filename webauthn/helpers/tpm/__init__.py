4d1550ec48eeabe9be8ce189224610d41d8e8a36
Hans-Wilhelm Warlo <5417271+warlo@users.noreply.github.com>
2023-02-11 16:53:06 +0100

    Add __all__

--- webauthn/helpers/tpm/__init__.py
+++ webauthn/helpers/tpm/__init__.py
@@ -1,2 +1,4 @@
-from .parse_cert_info import parse_cert_info  # noqa: F401
-from .parse_pub_area import parse_pub_area  # noqa: F401
+from .parse_cert_info import parse_cert_info
+from .parse_pub_area import parse_pub_area
+
+__all__ = ["parse_cert_info", "parse_pub_area"]
