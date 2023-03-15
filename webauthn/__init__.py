65e8b1ca4c2923ce60384b65e26a585e6d6b4ed4
Matthew Miller <mmiller@duosecurity.com>
2023-03-15 15:01:00 -0700

    Bump version to v1.8.0

--- webauthn/__init__.py
+++ webauthn/__init__.py
@@ -8,7 +8,7 @@ from .authentication.verify_authentication_response import (
 )
 from .helpers import base64url_to_bytes, options_to_json
 
-__version__ = "1.7.2"
+__version__ = "1.8.0"
 
 __all__ = [
     "generate_registration_options",
