67dc43f14f75294712f4ab6c024169e326fa0983
Matthew Miller <mmiller@duosecurity.com>
2023-09-29 09:57:56 -0700

    Support `dict`'s for `credential` during registration

--- webauthn/helpers/parse_registration_credential_json.py
+++ webauthn/helpers/parse_registration_credential_json.py
@@ -1,15 +1,23 @@
-from typing import Callable
+import json
+from typing import Callable, Union
 from pydantic import ValidationError
 
 from .exceptions import InvalidRegistrationResponse
 from .structs import PYDANTIC_V2, RegistrationCredential
 
 
-def parse_registration_credential_json(json_val: str) -> RegistrationCredential:
+def parse_registration_credential_json(json_val: Union[str, dict]) -> RegistrationCredential:
+    """
+    Parse a JSON form of a registration credential, as either a stringified JSON object or a
+    plain dict, into an instance of RegistrationCredential
+    """
     if PYDANTIC_V2:
         parsing_method: Callable = RegistrationCredential.model_validate_json  # type: ignore[attr-defined]
     else:  # assuming V1
         parsing_method = RegistrationCredential.parse_raw
+
+    if isinstance(json_val, dict):
+        json_val = json.dumps(json_val)
 
     try:
         registration_credential = parsing_method(json_val)
