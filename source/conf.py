a90823ebf5204a769203f375d2425a549a9a38dc
Matthew Miller <mmiller@duosecurity.com>
2024-08-15 09:56:49 -0700

    Run Black over source/

--- source/conf.py
+++ source/conf.py
@@ -6,26 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'py_webauthn'
-copyright = '2020, Duo Labs'
-author = 'Duo Labs'
-release = '2020'
+project = "py_webauthn"
+copyright = "2020, Duo Labs"
+author = "Duo Labs"
+release = "2020"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ["myst_parser"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
+html_copy_source = False
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 # -- Extension Configuration - MyST Parser
 
